8f11d50b48701f49ca4a71c550275b6c75a073b7
Glauber <glauberdribeiro@gmail.com>
2022-09-15 10:31:26 -0300

    Landing Page Nlw
    Trilha Ignite

---
+++ esports/web/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      
+      backgroundImage: {
+        background: "url('/background.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.88%, #43E7AD 33.94%, #E1D55d 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)', 
+      }
+    },
+  },
+  plugins: [],
+}
